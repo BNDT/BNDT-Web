a6597aa29db0392d34c8b13723a1e2c96fb72218
trungtin0012 <trungtinnguyen1994@gmail.com>
2016-06-28 23:40:19 +0700

    Correct widget name
    Change widget name from suprot to support

--- wordpress/wp-content/themes/zerif-lite/functions.php
+++ wordpress/wp-content/themes/zerif-lite/functions.php
@@ -1270,24 +1270,24 @@ function zerif_lite_update_options_in_pirate_forms() {
 } 
 //khởi tạo widget item
 
-add_action('widgets_init','suport_widget');
-    function suport_widget() 
+add_action('widgets_init','support_widget');
+    function support_widget() 
     {
-        register_widget('Supprot_Client');
+        register_widget('Support_Client');
 
     } 
 /**
-* tạo class Supprot_Client
+* tạo class Support_Client
 */
-class Supprot_Client extends WP_Widget
+class Support_Client extends WP_Widget
 {
     
     function __construct()
     {
         # code...
         parent::__construct(
-               'supprot_client',
-               'Supprot Client' ,
+               'support_client',
+               'Support Client' ,
                 array('description' => 'Forms Hộ Trợ Khách Hàng')
             );
     }
